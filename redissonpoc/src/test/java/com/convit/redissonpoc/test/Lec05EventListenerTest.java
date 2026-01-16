734654a84b81ccc5c288ba5c9fdebcd7228cdf11
Viet-Hoang Pham <157110579+HemlockPham7@users.noreply.github.com>
2026-01-16 18:47:58 +0700

    event listener

---
+++ redissonpoc/src/test/java/com/convit/redissonpoc/test/Lec05EventListenerTest.java
@@ -0,0 +1,40 @@
+package com.convit.redissonpoc.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.DeletedObjectListener;
+import org.redisson.api.ExpiredObjectListener;
+import org.redisson.api.RBucketReactive;
+import org.redisson.client.codec.StringCodec;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.TimeUnit;
+
+public class Lec05EventListenerTest extends BaseTest {
+
+    @Test
+    public void expiredEventTest() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam", 10, TimeUnit.SECONDS);
+        Mono<Void> get = bucket.get()
+                .doOnNext(System.out::println)
+                .then();
+        Mono<Void> event = bucket.addListener(new ExpiredObjectListener() {
+            @Override
+            public void onExpired(String s) {
+                System.out.println("Expired: " + s);
+            }
+        }).then();
+        // config set notify-keyspace-events AKE
+
+//        Mono<Void> event = bucket.addListener(new DeletedObjectListener() {
+//            @Override
+//            public void onDelete(String s) {
+//                System.out.println("Expired: " + s);
+//            }
+//        }).then();
+        StepVerifier.create(set.concatWith(get).concatWith(event))
+                .verifyComplete();
+        sleep(5000);
+    }
+}
