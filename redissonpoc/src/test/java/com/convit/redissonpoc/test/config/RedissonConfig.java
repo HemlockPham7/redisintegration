078b65781af3c3a8d53bcd0b3ce88bbbe62a2aef
Viet-Hoang Pham <157110579+HemlockPham7@users.noreply.github.com>
2026-01-16 11:55:48 +0700

    remove junit libraries and simple key value store

--- redissonpoc/src/test/java/com/convit/redissonpoc/test/config/RedissonConfig.java
+++ redissonpoc/src/test/java/com/convit/redissonpoc/test/config/RedissonConfig.java
@@ -1,6 +1,8 @@
 package com.convit.redissonpoc.test.config;
 
+import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
 import org.redisson.config.Config;
 
 import java.util.Objects;
@@ -12,7 +14,14 @@ public class RedissonConfig {
         public RedissonClient getClient() {
             if (Objects.isNull(this.redissonClient)) {
                 Config config = new Config();
-
+                config.useSingleServer()
+                        .setAddress("redis://127.0.0.1:6379");
+                redissonClient = Redisson.create(config);
             }
+            return redissonClient;
+        }
+
+        public RedissonReactiveClient getReactiveClient() {
+            return getClient().reactive();
         }
 }
